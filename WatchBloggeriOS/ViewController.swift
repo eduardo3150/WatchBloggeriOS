8bae6f5df2ba629f6c3126bef76408e310e23ec2
Eduardo Chavez <mccartney3150@gmail.com>
2020-12-19 21:15:48 -0600

    used swiftlint

--- WatchBloggeriOS/ViewController.swift
+++ WatchBloggeriOS/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  WatchBloggeriOS
-//
-//  Created by Eduardo Chavez on 12/18/20.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
